8a4670b79197bb85380e7b9db3487a578dca854c
lakshadvani <laksh.advani@gmail.com>
2018-04-16 21:45:59 -0600

    Update Pagerank.scala

--- spark/src/main/scala/spark/Pagerank.scala
+++ spark/src/main/scala/spark/Pagerank.scala
@@ -1,70 +1,45 @@
-/* PageRank.scala */
 package spark
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 
-object PageRank {
+object Pagerank extends App {
 
-  private var DEFAULT_ITERATION = 60
-  private var DEFAULT_BETA = 0.8
+  // creating SparkContext
+  val conf = new SparkConf().setAppName("Page Rank web-Google file").setMaster("local[*]")
+  val sc = new SparkContext(conf)
 
-  def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Page Rank")
-    val sc = new SparkContext(conf)
+  // point to Google file
+  val data = sc.textFile("web-Google.txt")
 
-    if(args.length < 3) {
-      println("Usage: spark-submit --class <className> --master <masterNode> <jar location> <input location> <output location> <modified/simple> <opt: number iterations> <opt: beta>")
-      sc.stop()
-    }
-    //if user input number of iteration for page rank, we override the default.
-    if(args.length > 3) {
-      PageRank.DEFAULT_ITERATION = args(3).toInt
-    }
-    //if user input the beta value, we override the default.
-    if(args.length > 4) {
-      PageRank.DEFAULT_BETA = args(4).toDouble
-    }
+  // creating links among pages from file using cache to improve performance
+  val links = data.filter(line=>(!line.startsWith("#"))).map(s=>s.split("\\s+")).map(ids=>(ids(0),ids(1))).distinct().groupByKey().cache()
 
-    val webLinksData = sc.textFile(args(0))
+  // init rank for every page
+  var ranks = links.mapValues(v => 1.0)
 
-    //emit a pair (fromPage, toPage) entry for each webpage linkage relationship
-    val links = webLinksData.filter(line => !line.matches("#.*")).map(s => {
-      val pairs = s.split("\t")
-      (pairs(0), pairs(1))
-    }).distinct().groupByKey().cache()
+  // declare number iterations
+  val numIter = 10
 
-    //initialise the rank to 1/N
-    val numWebpages = links.count();
-    var ranks = links.mapValues(v => 1.0/ numWebpages)
+  // loop to calculate and update the rank for each page
+  // limited by numIter iterations
+  for (i <- 1 to numIter) {
 
-    for (i <- 1 to PageRank.DEFAULT_ITERATION) {
-      /* links join ranks will give (fromWebId, ([list of toWebIds], rank of fromWebId))
-       * By the page rank formula, each toWebpages obtain below weighted rank from fromWebPage
-       * = (rank of fromWebPage) / num of outgoing links in fromWebPage
-       */
-      val ranks_calculated_based_on_fromPages = links.join(ranks).values.flatMap(toNodesAndFromPageRank => {
-        val fromPageRank = toNodesAndFromPageRank._2
-        val numOutgoingLinksInFromPage = toNodesAndFromPageRank._1.size
-        val toPagesList = toNodesAndFromPageRank._1
-        toPagesList.map(toPage => {
-          val rank_contributedBy_fromPage = fromPageRank / numOutgoingLinksInFromPage;
-          (toPage, rank_contributedBy_fromPage)
-        })
-      })
-      if(args(2).equalsIgnoreCase("modified")) {
-        // There are different version of PageRank algorithm calculation.
-        // It is referring to formula PR(A) = (1-d) / N + d (PR(T1)/C(T1) + ... + PR(Tn)/C(Tn)) in https://en.wikipedia.org/wiki/PageRank
-        ranks = ranks_calculated_based_on_fromPages.reduceByKey(_ + _).mapValues(rank=> PageRank.DEFAULT_BETA * rank + (1 - PageRank.DEFAULT_BETA)/numWebpages)
-      } else {
-        ranks = ranks_calculated_based_on_fromPages.reduceByKey(_ + _)
-      }
-    }
+    // join pages and ranks RDDs
+    val infopages = links.join(ranks)
 
-    val result = ranks.map(pageRankResultEntry =>pageRankResultEntry._1 + "\t" + pageRankResultEntry._2)
-    result.repartition(1).saveAsTextFile(args(1) + args(2) + " PageRank")
+    // calculating ranks for each tuple (link,rank)
+    val inforanks = infopages.values.flatMap{ case (links, rank) => links.map(link => (link, rank / links.size)) }
 
-    sc.stop()
+    // updating rank with formula given and using cache to update ranks
+    ranks = inforanks.reduceByKey((x,y)=> x+y).mapValues(value=> value*0.85  + 0.15).cache()
+    //ranks.leftOuterJoin(updatedranks)
   }
+
+  // sort result pages by rank
+  val result = ranks.map(_.swap).sortByKey(false).map(_.swap)
+
+  // save result as "resultado"
+  result.saveAsTextFile("resultado")
+
 }
