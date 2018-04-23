059785a0b5b1c81bb029d937947ddd09ba9a584d
Hasil Sharma <hasilsharma7@gmail.com>
2018-04-23 11:41:51 -0600

    Update TwitterLiveStreamConfig.scala

--- mapreduce/src/main/scala/mapreduce/utils/TwitterLiveStreamConfig.scala
+++ mapreduce/src/main/scala/mapreduce/utils/TwitterLiveStreamConfig.scala
@@ -1,11 +1,11 @@
 package mapreduce.utils
 
 object TwitterLiveStreamConfig {
-  val ACCESS_TOKEN_SECRET: String = "7KAva5NHSc1ExAf3Ci3cmJFxwsQU4nlkoIIyHl9nCUeUc"
+  val ACCESS_TOKEN_SECRET: String = 
 
-  val ACCESS_TOKEN: String = "1327782752-CnVRtLcEOLuOBlYhhFowfjli2bvjWqrSPbWxVgx"
+  val ACCESS_TOKEN: String = 
 
-  val CONSUMER_SECRET: String = "6fujwa7Q3nAJdhEjH20NSMdqdRWzzQWXPAGsbYkIZ3qIvnMiOT"
+  val CONSUMER_SECRET: String = 
 
-  val CONSUMER_KEY: String = "ibKVFwn6prLt6uHlOmvs1cTBu"
+  val CONSUMER_KEY: String =
 }
